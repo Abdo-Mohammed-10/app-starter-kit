00718ce44682079016ddda9400f49686bb1211c5
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2025-09-06 22:28:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 from PIL import Image
 import os
 import mimetypes
-from google import genai
-from google.genai import types
 
 # -----------------------------
 # Gemini API Key
